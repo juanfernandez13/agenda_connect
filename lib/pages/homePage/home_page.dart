1b5e7c0797237f311e174003bdbfdb1adac6ccd2
Juan Pablo <juanp1310@outlook.com>
2023-07-24 13:29:48 -0300

    :lipstick: feat: Estilizaçao da homePage

--- lib/pages/homePage/home_page.dart
+++ lib/pages/homePage/home_page.dart
@@ -41,47 +41,98 @@ class _HomePageState extends State<HomePage> {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         )),
-        child: Column(
+        child: Stack(
           children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                "Contatos",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
-              ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Container(
+                      margin: const EdgeInsets.only(top: 200),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(500), topRight: Radius.circular(500),)
+                      ),
+                      width: 100,
+                      height: 200,
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 50, right: 50, top: 50),
+                      decoration: const BoxDecoration(
+                        color: Color(0xff00DFFD),
+                        borderRadius: BorderRadius.only(bottomRight: Radius.circular(500), topRight: Radius.circular(500))
+
+                      ),
+                    ),
+                    ),
+                    Expanded(child: SizedBox(),),
+
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(500), )
+                      ),
+                      width: 200,
+                      height: 200,
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 60, left: 60),
+                      decoration: const BoxDecoration(
+                        color: Color(0xff00DFFD),
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(500), )
+
+                      ),
+                    ),
+                    ),
+                   
+                  ],
+                )
+              ],
             ),
-            DropdownButton(
-              value: dropDownValue,
-              items: opcoes.map((e) {
-                return DropdownMenuItem(
-                  value: e,
-                  child: Text(e),
-                );
-              }).toList(),
-              onChanged: (String? value) {
-                setState(() {
-                  dropDownValue = value ?? "Todos os contatos";
-                });
-              },
-            ),
-            Expanded(
-                child: ListView(
-                    children: contatosList.map((contato) {
-              return DetailsContact(contatoModel: contato);
-            }).toList())),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => CadastroPage()));
-                },
-                child: const CircleAvatar(
-                  radius: 30,
-                  child: Icon(Icons.add),
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    "Contatos",
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
+                  ),
                 ),
-              ),
-            )
+                DropdownButton(
+                  value: dropDownValue,
+                  items: opcoes.map((e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {
+                    setState(() {
+                      dropDownValue = value ?? "Todos os contatos";
+                    });
+                  },
+                ),
+                Expanded(
+                    child: ListView(
+                        children: contatosList.map((contato) {
+                  return DetailsContact(contatoModel: contato);
+                }).toList())),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => CadastroPage()));
+                    },
+                    child: const CircleAvatar(
+                      backgroundColor: Color(0xff00DFFD),
+                      radius: 30,
+                      child: Icon(Icons.add, color: Colors.black,),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
